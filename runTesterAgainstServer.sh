2a50d5b4552fb3151f5fa6a87c0948c23bc83d0e
Michiel de Jong <michiel@unhosted.org>
2020-06-12 21:37:17 +0200

    Remove Trellis reference in runTesterAgainstServer.sh

--- runTesterAgainstServer.sh
+++ runTesterAgainstServer.sh
@@ -6,14 +6,6 @@ docker build -t $2 servers/$2
 echo Starting server ...
 docker run -d --name=server --network=testnet $2
 
-if [[ "$2" == trellis ]]
-  then
-    docker logs server
-    echo Waiting for ten seconds ...
-    sleep 10
-    docker logs server
-fi
-
 echo Running $1 tester interactively ...
 docker run --name tester --network=testnet -it $1 /bin/bash
 
