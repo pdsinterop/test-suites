bd98d7827ed9cc15f5d0fa6caa0e05091157633f
Michiel de Jong <michiel@unhosted.org>
2020-06-23 12:43:48 +0200

    test on port 443

--- testers/webid-provider/tester/test/surface/fetch-openid-config.test.ts
+++ testers/webid-provider/tester/test/surface/fetch-openid-config.test.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 test("/.well-known/openid-configuration is valid JSON", async () => {
   const result = await fetch(
-    "https://server:8443/.well-known/openid-configuration"
+    "https://server/.well-known/openid-configuration"
   );
   expect(result.status).toEqual(200);
   const body = await result.text();
