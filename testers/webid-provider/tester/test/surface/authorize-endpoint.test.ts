fa0f4a001316f2e1e3a41d605c758d196aa57a88
Michiel de Jong <michiel@unhosted.org>
2020-06-30 12:57:48 +0200

    simplify authorizationEndpoint var

--- testers/webid-provider/tester/test/surface/authorize-endpoint.test.ts
+++ testers/webid-provider/tester/test/surface/authorize-endpoint.test.ts
@@ -27,18 +27,9 @@ describe("The server's authorize endpoint", () => {
     );
     const body = await configFetchResult.text();
     const configObj = JSON.parse(body);
-    const authorizeEndpoint = configObj.authorization_endpoint;
-    const params = [
-      [ 'response_type', 'id_token code'],
-      [ 'redirect_uri', 'http://localhost:3001/redirect'],
-      [ 'scope', 'openid profile offline_access'],
-      [ 'client_id', 'coolApp'],
-      [ 'code_challenge_method', 'S256'],
-      [ 'code_challenge', 'M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc'],
-      [ 'state', '84ae2b48-eb1b-4000-8782-ac1cd748aeb0']
-    ];
     authorizationEndpoint = configObj.authorization_endpoint;
     cookie = await getCookie();
+
   });
 
   test("the authorize endpoint is within the system under test", async () => {
@@ -111,12 +102,12 @@ describe("The server's authorize endpoint", () => {
       redirect: "manual"
     });
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location')).toEqual(`${SERVER_ROOT}/authorize?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`);
+    expect(fetchResult.headers.get('location')).toEqual(`${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`);
   });
 
   test("Authorize with cookie after consent redirects you back to the app", async () => {
     // This test uses the consented coolApp2
-    const fetchResult = await fetch(`${SERVER_ROOT}/authorize?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`, {
+    const fetchResult = await fetch(`${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`, {
       "headers": {
         cookie
       },
