d3a288cc4c1cf546ec5ae9cbe0f7bfb28814ae4a
Michiel de Jong <michiel@unhosted.org>
2020-06-30 13:52:16 +0200

    check JWT signature on id_token

--- testers/webid-provider/tester/test/surface/token.test.ts
+++ testers/webid-provider/tester/test/surface/token.test.ts
@@ -1,9 +1,10 @@
-import { decode } from "jsonwebtoken";
+import { decode, verify } from "jsonwebtoken";
 import Debug from "debug";
 import fetch, { Response } from "node-fetch";
-import RSA from "node-rsa";
-import { URL } from "url";
 
+import { subtle } from "isomorphic-webcrypto";
+import base64url from "base64url";
+import * as RSA from "node-rsa";
 
 const debug = Debug('token tests');
 
@@ -11,6 +12,19 @@ const ALICE_WEBID = process.env.ALICE_WEBID;
 const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
 const LOGIN_URL = `${SERVER_ROOT}/login/password`;
 const query2 = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp2&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
+
+function hashClaim (value, hashLength) {
+  if (value) {
+    let alg = { name: `SHA-${hashLength}`}
+    let octets = new Buffer(value, 'ascii')
+
+    return subtle.digest(alg, new Uint8Array(octets)).then(digest => {
+      let hash = Buffer.from(digest)
+      let half = hash.slice(0, hash.byteLength / 2)
+      return base64url(half)
+    })
+  }
+}
 
 async function getCookie() {
   const result = await fetch(`${SERVER_ROOT}/login/password`, {
@@ -104,8 +118,37 @@ describe("The IODC token", () => {
   test("id token has a JWT ID", async () => {
     expect(typeof idTokenObj.jti).toEqual("string");
   });
-  test("id token has a code hash", async () => {
-    console.log(idTokenObj);
-    expect(typeof idTokenObj.c_hash).toEqual("string");
+  test("id token has the correct c_hash", async () => {
+    expect(idTokenObj.c_hash).toEqual(await hashClaim(code, 256));
+  });
+  test("id token was signed by issuer", async () => {
+    // console.log(jwks.keys.map(key => key.kid));
+    let found = false;
+    jwks.keys.forEach((key) => {
+      if(found) {
+        return;
+      }
+      const pubKeyComponents = {
+        e: Buffer.from(key.e, 'base64'),
+        n: Buffer.from(key.n, 'base64')
+      }
+      debug('pubKeyComponents', pubKeyComponents)
+      const rsaPubKey: RSA = new RSA()
+      rsaPubKey.importKey(pubKeyComponents, 'components-public')
+      const publicPem: string = rsaPubKey.exportKey('pkcs1-public-pem')
+      debug('publicPem', publicPem)
+      try {
+        const result = verify(idTokenJwt, publicPem, {
+          algorithms: [ "RS256" ]
+        });
+        // console.log(result, key.kid, 'yes');
+        found = true;
+      } catch (e) {
+        // console.log('no', key.kid, e.message);
+        // ignore, probably this wasn't the right kid.
+      }
+    });
+    // console.log({ found });
+    expect(found).toEqual(true);
   });
 });
