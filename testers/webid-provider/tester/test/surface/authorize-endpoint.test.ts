c1cd440ec3c8b27637762406d106c6246c2fd697
Michiel de Jong <michiel@unhosted.org>
2020-06-30 10:45:40 +0200

    5 passing tests for authorize

--- testers/webid-provider/tester/test/surface/authorize-endpoint.test.ts
+++ testers/webid-provider/tester/test/surface/authorize-endpoint.test.ts
@@ -1,7 +1,9 @@
 import fetch from "node-fetch";
+import { hasUncaughtExceptionCaptureCallback } from "process";
 
 const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
 const LOGIN_URL = `${SERVER_ROOT}/login/password`;
+const query = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
 
 async function getCookie() {
   const result = await fetch("https://localhost/login/password", {
@@ -44,25 +46,46 @@ describe("The server's authorize endpoint", () => {
     expect(authorizationEndpoint.startsWith(SERVER_ROOT)).toEqual(true);
   });
 
-  test.skip("the authorize URL with cookie works", async () => {
-    const fetchResult = await fetch(authorizationEndpoint, {
-      headers: {
-        cookie
-      }
+  test("the authorize URL without cookie sends you to login", async () => {
+    const fetchResult = await fetch(authorizationEndpoint + query, {
+      redirect: "manual"
     });
-    expect(fetchResult.status).toEqual(200);
-    const body = await fetchResult.text();
-    expect(body.indexOf("form")).not.toEqual(-1);
-  }); 
+    expect(fetchResult.status).toEqual(302);
+    expect(fetchResult.headers.get('location')).toEqual("https://localhost:443/login" + query);
+  });
 
-  test("the authorize URL without cookie presents a login form", async () => {
-    const query = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
+  test("when redirected to login, you see a html form", async () => {
     const fetchResult = await fetch(authorizationEndpoint + query, {
       redirect: "follow"
     });
     expect(fetchResult.status).toEqual(200);
-    // expect(fetchResult.headers.get('location').startsWith("https://")).toEqual(true);
     const body = await fetchResult.text();
     expect(body.indexOf("form")).not.toEqual(-1);
   });
+
+  test("the authorize URL with cookie sends you to consent", async () => {
+    const fetchResult = await fetch(authorizationEndpoint + query, {
+      headers: {
+        cookie
+      },
+      redirect: "manual"
+    });
+    expect(fetchResult.status).toEqual(302);
+    expect(fetchResult.headers.get('location')).toEqual("https://localhost:443/sharing" + query);
+    const body = await fetchResult.text();
+    console.log(body);
+  });
+
+  test("when redirected to consent, there is a html form", async () => {
+    const fetchResult = await fetch(authorizationEndpoint + query, {
+      headers: {
+        cookie
+      },
+      redirect: "follow"
+    });
+    expect(fetchResult.status).toEqual(200);
+    const body = await fetchResult.text();
+    expect(body.indexOf("form")).not.toEqual(-1);
+  });
+
 });
