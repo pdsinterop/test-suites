98b985a399205f6fbd3bb96deb67aa29818418d1
Michiel de Jong <michiel@unhosted.org>
2020-08-10 13:24:14 +0200

    Add open-cloud-mesh tester

---
+++ testers/open-cloud-mesh/tester/test/surface/discovery.test.ts
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
+
+describe("Discovery at /ocm-provider/", () => {
+  test("contains service description", async () => {
+    const fetched = await fetch(`${SERVER_ROOT}/ocm-provider/`);
+    const jsonParsed = await fetched.json();
+    expect(jsonParsed).toEqual({
+      "enabled":true,
+      "apiVersion":"1.0-proposal1",
+      "endPoint":"http://localhost/server/index.php/ocm",
+      "resourceTypes": [
+        {
+          "name": "file",
+          "shareTypes": ["user", "group"],
+          "protocols": {
+            "webdav":"http://localhost/server/public.php/webdav/"
+          }
+        }
+      ],
+    });
+
+});
