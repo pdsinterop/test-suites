61d6b6e6e864198e32ed94c476afddf01f24e2aa
Michiel de Jong <michiel@unhosted.org>
2020-09-10 16:39:48 +0200

    Fix mod_rewrite rule

--- servers/nextcloud-server/init.sh
+++ servers/nextcloud-server/init.sh
@@ -2,4 +2,4 @@
 php console.php maintenance:install --admin-user alice --admin-pass alice123
 php console.php status
 php console.php app:enable solid
-sed -i '66 i\  RewriteRule ^\\.well-known/openid-configuration /app/solid/openid.php [QSA,L]' .htaccess
+sed -i '61 i\  RewriteRule ^\\.well-known/openid-configuration /apps/solid/openid [R=302,L]' .htaccess
