76854f1edca8734848810cbbee96928789b7cd5a
Michiel de Jong <michiel@unhosted.org>
2020-09-10 15:18:43 +0200

    Use openid-configuration branch of solid-nextcloud app

--- servers/nextcloud-server/init.sh
+++ servers/nextcloud-server/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-php console.php maintenance:install --admin-user alice --admin-pass nextcloud123
+php console.php maintenance:install --admin-user alice --admin-pass alice123
 php console.php status
-php console.php app:enable pdsinterop
+php console.php app:enable solid
+
