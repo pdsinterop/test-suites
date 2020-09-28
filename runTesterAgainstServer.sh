c2d890f2e48d9bcd699fdf8c57494f4ee5850682
Michiel de Jong <michiel@unhosted.org>
2020-09-28 15:26:38 +0200

    use 3-image structure in runTesterAgainstServer

--- runTesterAgainstServer.sh
+++ runTesterAgainstServer.sh
@@ -1,13 +1,22 @@
 echo Running tester $1 against server $2 ...
 echo Building tester and server images ...
 docker build -t $1 testers/$1
-docker build -t $2 servers/$2
+docker build -t $2-base servers/$2/base
+docker build -t $2 servers/$2/server
+docker build -t $2-cookie servers/$2/cookie
 
-echo Starting server ...
-docker run -d --name=server --rm --network=testnet $2
+echo Deleting stopped servers from previous run:
+docker rm server
+docker rm cookie
+docker rm tester
+
+echo Starting server...
+docker run -d --name=server --network=testnet $2
+echo Waiting for server to start ...
+sleep 2
+docker logs server
 if [[ "$2" == nextcloud-server ]]
   then
-    echo Waiting for Nextcloud server to start ...
     sleep 10
     docker logs server
     echo Running init script for Nextcloud server ...
@@ -15,8 +24,11 @@ if [[ "$2" == nextcloud-server ]]
     docker exec -u root -it server service apache2 reload
 fi
 
-echo Running $1 tester interactively ...
-docker run --cap-add=SYS_ADMIN --name tester --rm --env-file servers/$2/env.list --network=testnet -it $1 /bin/bash
+echo Getting cookie...
+export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --name cookie --env-file servers/$2/env.list $2-cookie`"
+
+echo Running $1 tester interactively with cookie $COOKIE...
+docker run --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$2/env.list -it $1 /bin/bash
 
 echo Tester exited, stopping server ...
 docker stop server
--- runTests.sh
+++ runTests.sh
@@ -4,12 +4,19 @@ docker build -t $1-base servers/$1/base
 docker build -t $1 servers/$1/server
 docker build -t $1-cookie servers/$1/cookie
 
+echo Deleting stopped servers from previous run:
+docker rm server
+docker rm cookie
+docker rm tester
+
 echo Starting server...
-docker run -d --name=server --rm --network=testnet $1
+docker run -d --name=server --network=testnet $1
 echo Waiting for server to start ...
-sleep 10
+sleep 2
+docker logs server
 if [[ "$1" == nextcloud-server ]]
   then
+    sleep 10
     docker logs server
     echo Running init script for Nextcloud server ...
     docker exec -u www-data -it server sh /init.sh
@@ -17,10 +24,10 @@ if [[ "$1" == nextcloud-server ]]
 fi
 
 echo Getting cookie...
-export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file servers/$1/env.list $1-cookie`"
+export COOKIE="`docker run --cap-add=SYS_ADMIN --network=testnet --name cookie --env-file servers/$1/env.list $1-cookie`"
 
 echo Running webid-provider tester with cookie $COOKIE...
-docker run --network=testnet --env COOKIE="$COOKIE" --env-file servers/$1/env.list webid-provider 2> reports/$1-webid-provider.txt
+docker run --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list webid-provider 2> reports/$1-webid-provider.txt
 
 # echo Running solid-crud tester...
 # docker run --network=testnet --env-file servers/$1/env.list solid-crud 2> reports/$1-solid-crud.txt
