2a776b34b5a6920766d9325cf443fc6687259a25
Michiel de Jong <michiel@unhosted.org>
2020-09-28 14:51:58 +0200

    Fix NSS cookie

--- servers/node-solid-server/cookie/app/index.js
+++ servers/node-solid-server/cookie/app/index.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
 const LOGIN_URL = `${SERVER_ROOT}/login/password`;
 const USERNAME = process.env.USERNAME || "alice";
-const PASSWORD = process.env.PASSWORD || "alice123";
+const PASSWORD = process.env.PASSWORD || "123";
 
 async function getCookie() {
   const result = await fetch(LOGIN_URL, {
