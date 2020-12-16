cc29b751e58d7fd810a2f345e1543c5b8c179733
Michiel de Jong <michiel@unhosted.org>
2020-12-16 14:47:12 +0100

    runOcmTests.sh

---
+++ runOcmTests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+function setup {
+  docker network create testnet
+  docker build -t solid-nextcloud servers/nextcloud-server/
+  docker build -t pubsub-server  https://github.com/pdsinterop/php-solid-pubsub-server.git#master
+  docker pull michielbdejong/nextcloud-cookie
+  docker pull solidtestsuite/webid-provider-tests:v1.2.0
+}
+function teardown {
+  docker stop `docker ps --filter network=testnet -q`
+  docker rm `docker ps --filter network=testnet -qa`
+  docker network remove testnet
+}
+
+function startSolidNextcloud {
+  docker run -d --name pubsub --network=testnet pubsub-server
+  docker run -d --name $1 --network=testnet --env-file ./env-vars-$1.list solid-nextcloud
+  until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI https://$1 2> /dev/null > /dev/null
+  do
+    echo Waiting for $1 to start, this can take up to a minute ...
+    docker ps -a
+    docker logs $1
+    sleep 1
+  done
+
+  docker logs $1
+  echo Running init script for Nextcloud $1 ...
+  docker exec -u www-data -it -e SERVER_ROOT=https://$1 $1 sh /init.sh
+  docker exec -u root -it $1 service apache2 reload
+  echo Getting cookie for $1...
+  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-$1.list michielbdejong/nextcloud-cookie`"
+}
+
+function runTests {
+  echo "Running $1 tests against server with cookie $COOKIE_server"
+  docker run --rm --network=testnet \
+    --env COOKIE="$COOKIE_server" \
+    --env COOKIE_ALICE="$COOKIE_server" \
+    --env COOKIE_BOB="$COOKIE_thirdparty" \
+    --env-file ./env-vars-testers.list solidtestsuite/$1-tests
+}
+
+# ...
+teardown || true
+setup
+startSolidNextcloud server
+runTests webid-provider
+runTests solid-crud
+# startSolidNextcloud thirdparty
+# runTests web-access-control
+teardown
--- runTests.sh
+++ runTests.sh
@@ -37,3 +37,66 @@ docker run --network=testnet --env-file servers/$1/env.list solid-crud 2> report
 
 echo Stopping server...
 docker stop server
+#!/bin/bash
+set -e
+
+echo Testing $1 ...
+echo Building image ...
+docker build -t $1 servers/$1
+
+echo Starting server ...
+docker run -d --name=server --env-file servers/$1/env.list --network=testnet $1
+
+# echo Starting idp ...
+# docker run --rm -d --name=idp --network=testnet node-solid-server
+
+if [[ "$1" == community-solid-server ]]; then
+    until docker run --rm --network=testnet webid-provider curl -kI http://server:3000 2> /dev/null > /dev/null
+    do
+      echo Waiting for server to start, this can take up to a minute ...
+      docker ps -a
+      docker logs server || true
+      sleep 1
+    done
+else
+    until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
+    do
+      echo Waiting for server to start, this can take up to a minute ...
+      docker ps -a
+      docker logs server || true
+      sleep 1
+    done
+fi
+
+if [[ "$1" == nextcloud-server ]]; then
+    echo Running init script for Nextcloud server ...
+    docker exec -u www-data -it server sh /init.sh
+    docker exec -u root -it server service apache2 reload
+fi
+
+if [[ "$1" == community-solid-server ]]; then
+  echo no cookie for community-solid-server
+else
+  echo Getting cookie...
+  export COOKIE="`docker run --rm --cap-add=SYS_ADMIN --network=testnet --name cookie -e SERVER_TYPE=$1 --env-file servers/$1/env.list cookie`"
+fi
+
+
+# echo Running rdf-fixtures tester ...
+# docker run --rm --network=testnet rdf-fixtures > reports/$1-rdf-fixtures.txt
+
+echo Running webid-provider tester with cookie $COOKIE...
+docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list webid-provider 2> reports/$1-webid-provider.txt
+
+echo Running solid-crud tester with cookie $COOKIE...
+docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list solid-crud 2> reports/$1-solid-crud.txt
+
+# echo Running web-access-control tester with cookie $COOKIE...
+# docker run --rm --network=testnet --name tester --env COOKIE="$COOKIE" --env-file servers/$1/env.list web-access-control 2> reports/$1-web-access-control.txt
+
+echo Stopping server and idp...
+docker stop server
+# docker stop idp
+
+echo Removing server...
+docker rm server
