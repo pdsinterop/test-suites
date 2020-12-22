db6c22df1d8d8d82e6adb80cb053c1d34ca60f40
Michiel de Jong <michiel@unhosted.org>
2020-12-22 16:10:39 +0100

    reset index.js as it was

--- helpers/cookie/app/index.js
+++ helpers/cookie/app/index.js
@@ -1,70 +1,41 @@
 const puppeteer = require("puppeteer");
+const solidAuthFetcher = require("solid-auth-fetcher");
 
-async function goPN_(serverRoot, username, password, shareWith) {
-  const loginUrl = `${serverRoot}/login`;
-  const filesUrl = `${serverRoot}/apps/files`;
-  const browser = await puppeteer.launch({ headless: false });
+const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
+const USERNAME = process.env.USERNAME || "alice";
+const PASSWORD = process.env.PASSWORD || "alice123";
+
+async function getCookieNextcloudCompatible() {
+  const LOGIN_URL = `${SERVER_ROOT}/login`;
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    headless: true,
+  });
   const page = await browser.newPage();
-  await page.goto(loginUrl);
-  console.log('on', loginUrl);
-  await page.type("#user", username);
-  await page.type("#password", password);
+  await page.goto(LOGIN_URL);
+  await page.type("#user", USERNAME);
+  await page.type("#password", PASSWORD);
   await page.click("#submit-form");
-  await page.waitForSelector('image.app-icon');
-  console.log('logged in');
-  await page.goto(filesUrl);
-  await page.waitForSelector('image.app-icon');
-  console.log('on', filesUrl);
-
-  async function go(selector) {
-    await page.waitForSelector(selector);
-    console.log(`Found ${selector}, clicking`);
-    await page.click(selector);  
-  }
-  await go('a.action-share');
-  // await go('button.action-item__menutoggle');
-  // await go('li.new-share-link');
-
-  await page.waitForSelector('a.sharing-entry__copy');
-  console.log('Found a.sharing-entry__copy');
-  const link = await page.evaluate("document.querySelector('a.sharing-entry__copy').getAttribute('href')");
-  console.log({ link });
-  await page.goto(link);
-
-  await go('button.menutoggle');
-  await go('button#save-external-share');
-  await page.type('#remote_address', shareWith);
-  await page.click('#save-button-confirm');
-  console.log('waiting for bit ...');
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const cookies = await page.cookies();
+  // console.log(cookies);
+  const cookieStr = cookies
+    .map(({ name, value }) => `${name}=${value}`)
+    .join("; ");
   await browser.close();
-}
-
-async function goP_N(serverRoot, username, password) {
-  const loginUrl = `${serverRoot}/login`;
-  const filesUrl = `${serverRoot}/apps/files`;
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(loginUrl);
-  console.log('on', loginUrl);
-  await page.type("#user", username);
-  await page.type("#password", password);
-  await page.click("#submit-form");
-  await page.waitForSelector('image.app-icon');
-  console.log('logged in');
-  await go('div.notifications-button');
-  await go('button.action-button.pull-right.primary');
-  // go to 'shared with you'
-  // remove the share so the test can be run again
-
-  console.log('waiting before closing');
-  await new Promise((resolve) => setTimeout(resolve, 20000));  
-  await browser.close();
+  return cookieStr;
 }
 
 async function run () {
-  await goPN_('https://nc2.pdsinterop.net', 'alice', 'alice123', 'alice@nc1.pdsinterop.net');
-  await goP_N('https://nc1.pdsinterop.net', 'alice', 'alice123');
+  let cookie
+  if (process.env.SERVER_TYPE === 'nextcloud-server') {
+    cookie = await getCookieNextcloudCompatible();
+  } else if (process.env.SERVER_TYPE === 'node-solid-server') {
+    cookie = await solidAuthFetcher.getNodeSolidServerCookie(SERVER_ROOT, USERNAME, PASSWORD);
+  } else if (process.env.SERVER_TYPE === 'php-solid-server') {
+    cookie = await solidAuthFetcher.getNodeSolidServerCookie(SERVER_ROOT, USERNAME, PASSWORD);
+  }
+  console.log(cookie);
 }
 
 // ..
