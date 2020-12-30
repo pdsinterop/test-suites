b75196aaab8152c9008ef344f69549ee38492ca3
Michiel de Jong <michiel@unhosted.org>
2020-12-30 15:12:14 +0100

    small cleanups

--- helpers/cookie/app/pnn.test.js
+++ helpers/cookie/app/pnn.test.js
@@ -9,41 +9,39 @@ const HEADLESS = !!process.env.HEADLESS;
 console.log({ HEADLESS });
 
 const flows = [
-  // 'Public link flow, log in first',
-  // 'Public link flow, log in after',
+  'Public link flow, log in first',
+  'Public link flow, log in after',
   'Share-with flow'
 ];
 const froms = [
-  // 'From Stub',
-  // 'From ownCloud',
+  'From ownCloud',
   'From Nextcloud'
 ];
 const tos = [
-  // 'To Stub',
   'To ownCloud',
-  // 'To Nextcloud'
+  'To Nextcloud'
 ];
 const params = {
   'From ownCloud': {
-    serverRoot: 'https://oc1.pdsinterop.net',
+    host: 'oc1.pdsinterop.net',
     guiType: GUI_TYPE_OWNCLOUD,
     username: 'admin',
     password: 'admin'
   },
   'To ownCloud': {
-    serverRoot: 'https://oc2.pdsinterop.net',
+    host: 'oc2.pdsinterop.net',
     guiType: GUI_TYPE_OWNCLOUD,
     username: 'admin',
     password: 'admin'
   },
   'From Nextcloud': {
-    serverRoot: 'https://nc1.pdsinterop.net',
+    host: 'nc1.pdsinterop.net',
     guiType: GUI_TYPE_NEXTCLOUD,
     username: 'alice',
     password: 'alice123'
   },
   'To Nextcloud': {
-    serverRoot: 'https://nc2.pdsinterop.net',
+    host: 'nc2.pdsinterop.net',
     guiType: GUI_TYPE_NEXTCLOUD,
     username: 'alice',
     password: 'alice123'
@@ -51,8 +49,8 @@ const params = {
 };
 
 class User {
-  constructor({ serverRoot, guiType, username, password }) {
-    this.serverRoot = serverRoot;
+  constructor({ host, guiType, username, password }) {
+    this.host = host;
     this.guiType = guiType;
     this.username = username;
     this.password = password;
@@ -66,10 +64,9 @@ class User {
   async login(fromCurrentPage) {
     const commonSteps = async ()=> {
       if (!fromCurrentPage) {
-        const loginUrl = `${this.serverRoot}/login`;
+        const loginUrl = `https://${this.host}/login`;
         await this.page.goto(loginUrl);
       }
-      // console.log('on', this.page.url());
       await this.type('#user', this.username);
       await this.type('#password', this.password);
     };
@@ -77,13 +74,11 @@ class User {
     if (this.guiType === GUI_TYPE_OWNCLOUD) {
       await commonSteps();
       await this.go('input.login');
-      // FIXME, work around https://github.com/michielbdejong/ocm-test-suite/issues/16 :
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await this.page.waitForNavigation();
     } else if (this.guiType === GUI_TYPE_NEXTCLOUD) {
       await commonSteps();
       await this.page.click("#submit-form");
       await this.page.waitForSelector('image.app-icon');
-      // console.log('logged in');
     } else if (this.guiType === GUI_TYPE_SEAFILE) {
       throw new Error('FIXME: https://github.com/michielbdejong/ocm-test-suite/issues/4');
     } else {
@@ -93,12 +88,10 @@ class User {
 
   async go(selector) {
     await this.page.waitForSelector(selector);
-    // console.log(`Found ${selector}, clicking`);
     await this.page.click(selector);
   }
   async type(selector, text) {
     await this.page.waitForSelector(selector);
-    // console.log(`Found ${selector}, typing "${text}"`);
     await this.page.type(selector, text);
   }
   async createPublicLink() {
@@ -109,11 +102,10 @@ class User {
       await this.go('span.icon-clippy-dark');
       return this.page.evaluate(() => navigator.clipboard.readText());
     } else if (this.guiType === GUI_TYPE_NEXTCLOUD) {
-      const filesUrl = `${this.serverRoot}/apps/files`;
+      const filesUrl = `https://${this.host}/apps/files`;
       await this.page.goto(filesUrl);
       // FIXME deal with first-time-use splash screen for Nextcloud Hub
       await this.page.waitForSelector('image.app-icon');
-      // console.log('on', this.page.url());
       await this.go('a.action-share');
 
       // FIXME: create public share for the first time
@@ -121,7 +113,6 @@ class User {
       // await this.go('li.new-share-link');
     
       await this.page.waitForSelector('a.sharing-entry__copy');
-      // console.log('Found a.sharing-entry__copy');
       return this.page.evaluate("document.querySelector('a.sharing-entry__copy').getAttribute('href')");
     } else if (this.guiType === GUI_TYPE_SEAFILE) {
       throw new Error('FIXME: https://github.com/michielbdejong/ocm-test-suite/issues/4');
@@ -136,19 +127,14 @@ class User {
       await this.go('li.subtab-localshare'); // sic
       await this.type('input.shareWithField', `${shareWithUser}@${shareWithHost}`);
       await this.go('div.share-autocomplete-item');
-      await new Promise((resolve) => setTimeout(resolve, 5000));
     } else if (this.guiType === GUI_TYPE_NEXTCLOUD) {
-      const filesUrl = `${this.serverRoot}/apps/files`;
+      const filesUrl = `https://${this.host}/apps/files`;
       await this.page.goto(filesUrl);
       // FIXME deal with first-time-use splash screen for Nextcloud Hub
       await this.page.waitForSelector('image.app-icon');
-      // console.log('on', this.page.url());
       await this.go('a.action-share');
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      // console.log('will type into', 'div.multiselect__tags input.multiselect__input', `${shareWithUser}@${shareWithHost}`);
       await this.type('div.multiselect__tags input.multiselect__input', `${shareWithUser}@${shareWithHost}`);
       await this.go('span.option__desc--lineone');
-      await new Promise((resolve) => setTimeout(resolve, 5000));
     } else if (this.guiType === GUI_TYPE_SEAFILE) {
       throw new Error('FIXME: https://github.com/michielbdejong/ocm-test-suite/issues/4');
     } else {
@@ -159,17 +145,13 @@ class User {
     await this.page.goto(url);
     if (remoteGuiType === GUI_TYPE_OWNCLOUD) {
       await this.go('button#save-button');
-      await this.page.type('#remote_address', this.serverRoot);
+      await this.page.type('#remote_address', this.host);
       await this.page.click('#save-button-confirm');
-      // console.log('Saved public link (from ownCloud GUI), now at', this.page.url());
-      // FIXME: avoid hard-coded timer here:
-      await new Promise((resolve) => setTimeout(resolve, 5000));
     } else if (remoteGuiType === GUI_TYPE_NEXTCLOUD) {
       await this.go('button.menutoggle');
       await this.go('button#save-external-share');
-      await this.page.type('#remote_address', `${this.username}@${this.serverRoot}`);
+      await this.page.type('#remote_address', `${this.username}@${this.host}`);
       await this.page.click('#save-button-confirm');
-      // console.log('Saved public link (from Nextcloud GUI), now at', this.page.url());
     } else if (remoteGuiType === GUI_TYPE_SEAFILE) {
       throw new Error('FIXME: https://github.com/michielbdejong/ocm-test-suite/issues/4');
     } else {
@@ -178,12 +160,8 @@ class User {
   }
   async acceptShare() {
     if (this.guiType === GUI_TYPE_OWNCLOUD) {
-      const sharedWithYouUrl = `${this.serverRoot}/apps/files/?dir=/&view=sharingin`;
-      // go to 'shared with you'
+      const sharedWithYouUrl = `https://${this.host}/apps/files/?dir=/&view=sharingin`;
       await this.page.goto(sharedWithYouUrl);
-      // FIXME: avoid hard-coded timer here:
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      // console.log('at', sharedWithYouUrl);
       await this.go('a.action-accept'); 
     } else if (this.guiType === GUI_TYPE_NEXTCLOUD) {
       await this.go('div.notifications-button');
@@ -196,22 +174,13 @@ class User {
   }
   async deleteAcceptedShare() {
     if (this.guiType === GUI_TYPE_OWNCLOUD) {
-      const sharedWithYouUrl = `${this.serverRoot}/apps/files/?dir=/&view=sharingin`;
-      // go to 'shared with you'
+      const sharedWithYouUrl = `https://${this.host}/apps/files/?dir=/&view=sharingin`;
       await this.page.goto(sharedWithYouUrl);
-      // FIXME: avoid hard-coded timer here:
-      // await new Promise((resolve) => setTimeout(resolve, 5000));
-      // console.log('at', sharedWithYouUrl);
       await this.go('span.icon-more');
-      // FIXME: avoid hard-coded timer here:
-      // await new Promise((resolve) => setTimeout(resolve, 5000));
       await this.go('a.action-delete');
     } else if (this.guiType === GUI_TYPE_NEXTCLOUD) {
-      const sharedWithYouUrl = `${this.serverRoot}/apps/files/?dir=/&view=sharingin`;
-        // go to 'shared with you'
+      const sharedWithYouUrl = `https://${this.host}/apps/files/?dir=/&view=sharingin`;
         await this.page.goto(sharedWithYouUrl);
-        // FIXME: avoid hard-coded timer here:
-        // await new Promise((resolve) => setTimeout(resolve, 5000));
         // remove the share so the test can be run again
         await this.go('a.action-menu');
         await this.go('li.action-delete-container');
@@ -223,8 +192,6 @@ class User {
   }
 
   async exit () {
-    // console.log('waiting for bit ...');
-    // await new Promise((resolve) => setTimeout(resolve, 10000));  
     await this.browser.close();
   }
 }
@@ -250,22 +217,12 @@ flows.forEach((flow) => {
             });
             
             it(to, async () => {
-              // console.log('1.2');
               await fromUser.login(false);
-              // console.log('1.3');
-              await fromUser.shareWith(params[to].username, params[to].serverRoot);
-              // console.log('1.4');
+              await fromUser.shareWith(params[to].username, params[to].host);
             
-              // console.log('2.3');
               await toUser.login(false);
-              // console.log('2.4');
               await toUser.acceptShare();
-              // console.log('2.5');
               await toUser.deleteAcceptedShare();
-              // console.log('2.6');
-            
-              // Not sure why this is necessary (is there an 'await' missing somewhere?):
-              await new Promise((resolve) => setTimeout(resolve, 1000));
             }, JEST_TIMEOUT);
           });
         } else {
