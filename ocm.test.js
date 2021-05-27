d04f961e78712490d7db582b68288882277d3e9a
Michiel de Jong <michiel@unhosted.org>
2021-05-27 13:37:53 +0200

    Hostnames as external parameters

--- ocm.test.js
+++ ocm.test.js
@@ -1,9 +1,9 @@
 const puppeteer = require("puppeteer");
-
-const GUI_TYPE_STUB = 'GUI Stub';
-const GUI_TYPE_OWNCLOUD = 'GUI ownCloud';
-const GUI_TYPE_NEXTCLOUD = 'GUI Nextloud';
-const GUI_TYPE_SEAFILE = 'GUI Seafile';
+const { GUI_TYPE_STUB,
+  GUI_TYPE_OWNCLOUD,
+  GUI_TYPE_NEXTCLOUD,
+  GUI_TYPE_SEAFILE,
+  params } = (process.env.LIVE ? require("./params-live") : require("./params-docker"));
 
 const JEST_TIMEOUT = 60000;
 const HEADLESS = !!process.env.HEADLESS;
@@ -26,44 +26,6 @@ const tos = [
   'To Nextcloud',
   'To Seafile',
 ];
-const params = {
-  'From Stub': {
-    host: 'stub1.pdsinterop.net',
-    guiType: GUI_TYPE_STUB,
-    username: 'admin',
-    password: 'admin'
-  },
-  'To Stub': {
-    host: 'stub1.pdsinterop.net',
-    guiType: GUI_TYPE_STUB,
-    username: 'admin',
-    password: 'admin'
-  },
-  'From ownCloud': {
-    host: 'oc1.pdsinterop.net',
-    guiType: GUI_TYPE_OWNCLOUD,
-    username: 'admin',
-    password: 'admin'
-  },
-  'To ownCloud': {
-    host: 'oc2.pdsinterop.net',
-    guiType: GUI_TYPE_OWNCLOUD,
-    username: 'admin',
-    password: 'admin'
-  },
-  'From Nextcloud': {
-    host: 'nc1.pdsinterop.net',
-    guiType: GUI_TYPE_NEXTCLOUD,
-    username: 'alice',
-    password: 'alice123'
-  },
-  'To Nextcloud': {
-    host: 'nc2.pdsinterop.net',
-    guiType: GUI_TYPE_NEXTCLOUD,
-    username: 'alice',
-    password: 'alice123'
-  }
-};
 
 class User {
   constructor({ host, guiType, username, password }) {
