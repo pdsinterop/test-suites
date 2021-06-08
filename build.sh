8cb0fbf95fc0802f01ed19661c6faf9734c1b536
Michiel de Jong <michiel@unhosted.org>
2021-06-08 16:20:14 +0200

    docker compose

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+docker build -t tester .
+
+git clone https://github.com/michielbdejong/ocm-stub
+cd ocm-stub
+docker build -t stub .
+cd ..
+rm -rf ocm-stub
+
 git clone https://github.com/cs3org/reva
 cd reva
 git checkout 56c89e07cf0c68d 
 docker build -t revadbase -f ./Dockerfile.revad .
 cd ..
 rm -rf reva
-docker build -t tester .
+
+
 cd revad
 docker build -t revad .
 cd ../servers/nextcloud-server
