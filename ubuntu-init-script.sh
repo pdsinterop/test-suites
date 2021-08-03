00cdab6ae8d340f5a057ac143596ef9cfbf46628
Michiel de Jong <michiel@unhosted.org>
2021-08-03 12:04:43 +0200

    approximation of #28

--- ubuntu-init-script.sh
+++ ubuntu-init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git
+apt-get install -y git vim
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source $NVM_DIR/nvm.sh
 nvm install 14
