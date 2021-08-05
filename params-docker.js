7251ec60a1ef1a2b657fca8a82e4a2514859ccaa
Michiel de Jong <michiel@unhosted.org>
2021-08-05 16:50:56 +0200

    GUI_TYPE_REVA

--- params-docker.js
+++ params-docker.js
@@ -3,6 +3,8 @@ const GUI_TYPE_STUB = 'GUI Stub';
 const GUI_TYPE_OWNCLOUD = 'GUI ownCloud';
 const GUI_TYPE_NEXTCLOUD = 'GUI Nextloud';
 const GUI_TYPE_SEAFILE = 'GUI Seafile';
+const GUI_TYPE_REVA = 'GUI Reva (i.e., reva-cli)';
+
 module.exports = {
   GUI_TYPE_STUB,
   GUI_TYPE_OWNCLOUD,
