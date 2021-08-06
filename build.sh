0abd910641f1b42db87fe7ec42fac5434b354a29
Michiel de Jong <michiel@unhosted.org>
2021-08-06 12:06:00 +0200

    Use v1.11.0 of reva

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd ..
 
 # git clone https://github.com/cs3org/reva
 cd reva
-git checkout 56c89e07cf0c68d 
+git checkout v1.11.0
 docker build -t revadbase -f ./Dockerfile.revad .
 cd ..
 # rm -rf reva
