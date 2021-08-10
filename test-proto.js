60ca2da4c10d797f76425e467891441a58118d31
Michiel de Jong <michiel@unhosted.org>
2021-08-10 16:56:59 +0200

    core access token not found

--- test-proto.js
+++ test-proto.js
@@ -1,6 +1,9 @@
 const grpc = require('grpc');
 const { GatewayAPIClient } = require('@cs3org/node-cs3apis/cs3/gateway/v1beta1/gateway_api_grpc_pb');
 const { AuthenticateRequest } = require('@cs3org/node-cs3apis/cs3/gateway/v1beta1/gateway_api_pb');
+const { ListReceivedOCMSharesRequest } = require('@cs3org/node-cs3apis/cs3/sharing/ocm/v1beta1/ocm_api_pb');
+ 
+
 
 const TARGET = process.env.TARGET || 'localhost:19000';
 
@@ -22,6 +25,20 @@ function authenticate(authType, clientId, clientSecret) {
   });
 }
 
+function ocmShareListReceived() {
+  const req = new ListReceivedOCMSharesRequest();
+  return new Promise((resolve, reject) => {
+    client.listReceivedOCMShares(req, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+
 async function example() {
   try {
     const res = await authenticate('basic', 'einstein', 'relativity');
@@ -29,6 +46,8 @@ async function example() {
     // * AuthenticateResponse https://github.com/cs3org/cs3apis/blob/a86e5cb6ac360/cs3/gateway/v1beta1/gateway_api.proto#L415
     // * User https://github.com/cs3org/cs3apis/blob/a86e5cb6ac360/cs3/identity/user/v1beta1/resources.proto#L53
     console.log(res.getUser().getDisplayName());
+    const shares = await ocmShareListReceived();
+    console.log(shares);
   } catch (e) {
     console.error(e);
   }
