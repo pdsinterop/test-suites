897f2ce855c409e265de35461ebe1300e4b6e505
Michiel de Jong <michiel@unhosted.org>
2021-08-12 16:11:31 +0200

    Read out all info about shares

--- reva-client.js
+++ reva-client.js
@@ -3,6 +3,7 @@ const grpc = require('grpc');
 const { GatewayAPIClient } = require('@cs3org/node-cs3apis/cs3/gateway/v1beta1/gateway_api_grpc_pb');
 const { AuthenticateRequest, WhoAmIRequest } = require('@cs3org/node-cs3apis/cs3/gateway/v1beta1/gateway_api_pb');
 const { ListReceivedOCMSharesRequest } = require('@cs3org/node-cs3apis/cs3/sharing/ocm/v1beta1/ocm_api_pb');
+const { GranteeType } = require('@cs3org/node-cs3apis/cs3/storage/provider/v1beta1/resources_pb');
 
 // Specifies the name of the Reva access token used during requests.
 // Align this string with the server expects, in the case of revad see:
@@ -62,7 +63,30 @@ module.exports = class RevaClient {
     const req = new ListReceivedOCMSharesRequest();
     // req.setToken(token);
     const shares = await this.grpcClient.listReceivedOCMShares(req, this.metadata);
-    console.log('SharesList from ListReceivedOCMSharesResponse:', shares.getSharesList()[0].getState(), shares.getSharesList()[0].getReferenceId());
+    shares.getSharesList().forEach(share => {
+      let idp, opaque;
+      if (share.getShare().getGrantee().getType() == GranteeType.GRANTEE_TYPE_USER) {
+        // console.log(share);
+
+        idp, opaque = share.getShare().getGrantee().getUserId().getIdp(), share.getShare().getGrantee().getUserId().getOpaqueId();
+      } else if (share.getShare().getGrantee().getType() == GranteeType.GRANTEE_TYPE_GROUP) {
+        idp, opaque = share.getShare().getGrantee().getGroupId().getIdp(), share.getShare().getGrantee().getGroupId().getOpaqueId();
+      }
+      console.log('share', [
+        share.getShare().getId().getOpaqueId(),
+        share.getShare().getOwner().getIdp(),
+        share.getShare().getOwner().getOpaqueId(),
+        share.getShare().getResourceId().getOpaqueId(),
+        share.getShare().getResourceId().getStorageId(),
+        share.getShare().getPermissions().getReshare(),
+        share.getShare().getGrantee().getType(),
+        idp,
+        opaque,
+        new Date(share.getShare().getCtime().getSeconds()),
+        new Date(share.getShare().getMtime().getSeconds()),
+        share.getState()
+        ]);  
+    });
   }
   
   async updateReceivedOCMShares(shareReference, newState) {
@@ -75,3 +99,25 @@ module.exports = class RevaClient {
     // console.log('updated', res);
   }
 }
+
+
+// const permFuncs = [
+  // 'getAddGrant',
+  // 'getCreateContainer',
+  // 'getDelete',
+  // 'getGetPath',
+  // 'getGetQuota',
+  // 'getInitiateFileDownload',
+  // 'getInitiateFileUpload',
+  // 'getListGrants',
+  // 'getListContainer',
+  // 'getListFileVersions',
+  // 'getMove',
+  // 'getRemoveGrant',
+  // 'getPurgeRecycle',
+  // 'getRestoreFileVersion',
+  // 'getRestoreRecycleItem',
+  // 'getStat',
+  // 'getUpdateGrant',
+  // 'getDenyGrant'
+// ];
