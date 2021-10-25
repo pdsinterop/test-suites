28ae4e2a79cba2a5bed02aebd13267fa4714b932
Michiel de Jong <michiel@unhosted.org>
2021-10-25 11:54:38 +0200

    no --rm and domains in params-docker.js

--- params-docker.js
+++ params-docker.js
@@ -15,24 +15,28 @@ module.exports = {
   params: {
     'From Stub': {
       host: 'stub1.docker',
+      domain: 'stub1.docker',
       guiType: GUI_TYPE_STUB,
       username: 'admin',
       password: 'admin'
     },
     'To Stub': {
       host: 'stub2.docker',
+      domain: 'stub2.docker',
       guiType: GUI_TYPE_STUB,
       username: 'admin',
       password: 'admin'
     },
     'From Reva': {
       host: 'revad1.docker', // https: port 443, grpc: port 19000
+      domain: 'revad1.docker',
       guiType: GUI_TYPE_REVA,
       username: 'einstein',
       password: 'relativity'
     },
     'To Reva': {
       host: 'revad2.docker', // https: port 443, grpc: port 19000
+      domain: 'revad2.docker',
       guiType: GUI_TYPE_REVA,
       username: 'marie',
       password: 'radioactivity'
@@ -51,12 +55,14 @@ module.exports = {
     // },
     'From Nextcloud': {
       host: 'nc1.docker',
+      domain: 'nc1.docker',
       guiType: GUI_TYPE_NEXTCLOUD,
       username: 'alice',
       password: 'alice123'
     },
     'To Nextcloud': {
       host: 'nc2.docker',
+      domain: 'nc2.docker',
       guiType: GUI_TYPE_NEXTCLOUD,
       username: 'alice',
       password: 'alice123'
