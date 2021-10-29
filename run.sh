5c9e81097e67d4bd0710fd9c240dce47e0e1f7df
Your Name <you@example.com>
2021-10-29 12:28:31 +0000

    typo

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker network create testnet
 docker run -d --network=testnet --rm --name=oc1.docker oc1
 docker run -d --network=testnet --rm --name=oc2.docker oc2
 docker run -d --network=testnet --rm --name=nc1.docker nc1
-docker run -d --network=testnet --rm --name=nc2.docker nc1
+docker run -d --network=testnet --rm --name=nc2.docker nc2
 docker run -d --network=testnet --rm --name=stub1.docker -e HOST=stub1 stub
 docker run -d --network=testnet --rm --name=stub2.docker -e HOST=stub2 stub
 docker run -d --network=testnet --rm --name=revad1.docker -e HOST=revad1 revad
