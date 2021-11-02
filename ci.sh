3ef0860778d5e22d913be1b01b012aac82b3df40
Your Name <you@example.com>
2021-11-02 10:31:27 +0000

    reduce duplication ubuntu-init/ci-script.sh

--- ci.sh
+++ ci.sh
@@ -11,4 +11,5 @@ docker run -d --network=testnet --rm --name=stub2.docker -e HOST=stub2 stub
 docker run -d --network=testnet --rm --name=revad1.docker -e HOST=revad1 revad
 docker run -d --network=testnet --rm --name=revad2.docker -e HOST=revad2 revad
 docker run -d --network=testnet --name=tester --cap-add=SYS_ADMIN tester
+docker exec tester /bin/bash /ubuntu-init-script.sh
 docker exec tester /bin/bash /ubuntu-ci-script.sh
