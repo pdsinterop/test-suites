b55a23225cf1b28a34d02fe4c4002db907997e10
Your Name <you@example.com>
2021-11-09 09:26:39 +0000

    run nc1,revanc1,revanc2,nc2

--- build.sh
+++ build.sh
@@ -21,35 +21,44 @@ createCert revanc2
 
 docker build -t tester .
 
+# image for stub1 and stub2:
 git clone https://github.com/michielbdejong/ocm-stub
 cd ocm-stub
 cp -r ../tls .
 docker build -t stub .
 cd ..
 
+# image for revad1, revad2, revanc1, revanc2:
 cd servers/revad
 cp -r ../../tls .
 docker build -t revad .
 
+# base image for nextcloud image and owncloud image:
 cd ../apache-php
 cp -r ../../tls .
 docker build -t apache-php .
 
+# base image for nc1 image and nc2 image:
 cd ../nextcloud
 docker build -t nextcloud .
 
+# image for nc1:
 cd ../nc1
 docker build -t nc1 .
 
+# image for nc2:
 cd ../nc2
 docker build -t nc2 .
 
+# base image for oc1 image and oc2 image:
 cd ../owncloud
 docker build -t owncloud .
 
+# image for oc1:
 cd ../oc1
 docker build -t oc1 .
 
+# image for oc2:
 cd ../oc2
 docker build -t oc2 .
 
