2a558f9a8b13ecc694143f3ba03f771acdd38a59
Your Name <you@example.com>
2021-12-09 15:51:44 +0000

    Set iopUrl in app config

--- run.sh
+++ run.sh
@@ -8,11 +8,15 @@ docker network create testnet
 
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc1.docker nc1
-docker exec -it -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity -u www-data nc1.docker sh /init.sh
-
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc2.docker nc2
+sleep 10
+docker exec -it -u www-data nc1.docker sh /init.sh
+# docker exec -it maria1.docker mariadb -u root -p1234 nextcloud -e "insert into oc_sciencemesh (apikey,sitename,siteurl,siteid,country,iopurl,numusers,numfiles,numstorage) values ('key','revanc1','https://nc1.docker','revanc1','NL','https://revanc1.docker',1,5,1);"
+docker exec -it maria1.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
+
 docker exec -it -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data nc2.docker sh /init.sh
+docker exec -it maria2.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc2.docker/');"
 
 #docker run -d --network=testnet --name=stub1.docker -e HOST=stub1 stub
 #docker run -d --network=testnet --name=stub2.docker -e HOST=stub2 stub
@@ -20,6 +24,7 @@ docker exec -it -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u w
 #docker run -d --network=testnet --name=revad2.docker -e HOST=revad2 revad
 docker run -d --network=testnet --name=revanc1.docker -e HOST=revanc1 revad
 docker run -d --network=testnet --name=revanc2.docker -e HOST=revanc2 revad
+
 docker run -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm --network=testnet --name=tester -d --cap-add=SYS_ADMIN tester
 
 TESTER_IP_ADDR=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tester`
--- servers/nextcloud/init.sh
+++ servers/nextcloud/init.sh
@@ -1,3 +1,5 @@
 php console.php maintenance:install --admin-user $USER --admin-pass $PASS --database "mysql" --database-name "nextcloud" --database-user "root" --database-pass "1234" --database-host "$DBHOST"
 php console.php app:disable firstrunwizard
 php console.php app:enable sciencemesh
+sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
+sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
