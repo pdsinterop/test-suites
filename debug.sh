adc8f147a56c46885e92d2875af721fb1e47ed19
Your Name <you@example.com>
2021-12-14 12:38:49 +0000

    debug.sh

---
+++ debug.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+docker stop `docker ps -q`
+docker rm `docker ps -aq`
+docker network remove testnet
+docker network create testnet
+
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet --name=nc1.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc1
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet --name=nc2.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc2
+docker run -d --network=testnet -v /root/reva:/reva --name=revanc1.docker -e HOST=revanc1 revad /bin/bash -c "trap : TERM INT; sleep infinity & wait"
+docker run -d --network=testnet -v /root/reva:/reva --name=revanc2.docker -e HOST=revanc2 revad /bin/bash -c "trap : TERM INT; sleep infinity & wait"
+sleep 10
+docker exec -it -u www-data nc1.docker sh /init.sh
+docker exec -it maria1.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
+
+docker exec -it -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data nc2.docker sh /init.sh
+docker exec -it maria2.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc2.docker/');"
+
+docker run -p 6080:80 -p 5900:5900 -v /dev/shm:/dev/shm --network=testnet --name=tester -d --cap-add=SYS_ADMIN tester
+
+TESTER_IP_ADDR=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tester`
+echo $TESTER_IP_ADDR
+# set up port forwarding from host to testnet for vnc:
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A PREROUTING -p tcp --dport 5900 -j DNAT --to-destination $TESTER_IP_ADDR:5900
+
+echo docker exec -it revanc1.docker /bin/bash
+echo docker exec -it revanc2.docker /bin/bash
+echo echo \"127.0.0.1 \$HOST.docker\" \>\> /etc/hosts
+echo export PATH=\$PATH:/usr/local/go/bin
+echo cd /reva \; make build-revad \; cd /etc/revad \; /reva/cmd/revad/revad -c /etc/revad/\$HOST.toml
