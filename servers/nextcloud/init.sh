9fce70f0fa0be55131dc723cbd1062072f9d7f5d
Michiel de Jong (Ponder Source) <michiel@pondersource.com>
2021-12-24 09:35:24 +0000

    sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php

--- servers/nextcloud/init.sh
+++ servers/nextcloud/init.sh
@@ -3,3 +3,4 @@ php console.php app:disable firstrunwizard
 php console.php app:enable sciencemesh
 sed -i "8 i\      1 => 'nc1.docker'," /var/www/html/config/config.php
 sed -i "9 i\      2 => 'nc2.docker'," /var/www/html/config/config.php
+sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
