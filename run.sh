8f18950d94b2a0c402a35a5b66a8fcb233df9b7c
Michiel de Jong <michiel@unhosted.org>
2022-01-12 15:06:58 +0100

    mysql creds

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ docker network create testnet
 #docker run -d --network=testnet --name=oc1.docker oc1
 #docker run -d --network=testnet --name=oc2.docker oc2
 
-docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc1.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc1
-docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=1234 --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc2.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc2
 sleep 10
 docker exec -it -u www-data nc1.docker sh /init.sh
-# docker exec -it maria1.docker mariadb -u root -p1234 nextcloud -e "insert into oc_sciencemesh (apikey,sitename,siteurl,siteid,country,iopurl,numusers,numfiles,numstorage) values ('key','revanc1','https://nc1.docker','revanc1','NL','https://revanc1.docker',1,5,1);"
-docker exec -it maria1.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
+# docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_sciencemesh (apikey,sitename,siteurl,siteid,country,iopurl,numusers,numfiles,numstorage) values ('key','revanc1','https://nc1.docker','revanc1','NL','https://revanc1.docker',1,5,1);"
+docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
 
 docker exec -it -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data nc2.docker sh /init.sh
-docker exec -it maria2.docker mariadb -u root -p1234 nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc2.docker/');"
+docker exec -it maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc2.docker/');"
 
 #docker run -d --network=testnet --name=stub1.docker -e HOST=stub1 stub
 #docker run -d --network=testnet --name=stub2.docker -e HOST=stub2 stub
