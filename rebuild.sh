42bd7b049df0d18c687ec18cdb7049ed5de12779
Your Name <you@example.com>
2022-02-22 15:56:43 +0000

    curl says subject name 'nc1' does not match target host name 'nc1.docker'

--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,7 @@ function createCert {
   openssl req -new -days 365 -nodes \
     -out ./tls/$1.csr \
     -keyout ./tls/$1.key \
-    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=$1" \
+    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=$1.docker" \
     -addext "subjectAltName = DNS:$1.docker"
   echo Signing CSR for $1.docker, creating cert.
   openssl x509 -req -in ./tls/$1.csr -CA ./tls/ocm-ca.crt -CAkey ./tls/ocm-ca.key -CAcreateserial -out ./tls/$1.crt
@@ -17,7 +17,7 @@ echo Generate CA self-signed certificate
 openssl req -new -x509 -days 365 \
     -key ./tls/ocm-ca.key \
     -out ./tls/ocm-ca.crt \
-    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=$1"
+    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=ocm-ca"
 
 createCert nc1
 createCert nc2
