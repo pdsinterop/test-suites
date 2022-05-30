edceb137cc02542ff4f727978d3004f0dec7aef2
Michiel de Jong <michiel@unhosted.org>
2022-05-30 12:54:26 +0200

    document 3 ways to run tester

--- setup.sh
+++ setup.sh
@@ -14,10 +14,5 @@ git clone https://github.com/michielbdejong/ocm-stub
 /bin/bash ./rebuild.sh
 /bin/bash ./debug.sh
 
-# TESTER_IP_ADDR=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' tester`
-# echo $TESTER_IP_ADDR
-# # set up port forwarding from host to testnet for vnc:
-# sysctl net.ipv4.ip_forward=1
-# iptables -t nat -A PREROUTING -p tcp --dport 5900 -j DNAT --to-destination $TESTER_IP_ADDR:5900
-
-docker run --network=testnet --name=tester.docker ci
+# There are really three ways to add a tester to the testnet,
+# see debug.sh for more details.
