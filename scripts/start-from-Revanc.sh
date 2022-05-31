20fae66efdffa4e63acd75aa32321d3fc4d8fd09
Your Name <you@example.com>
2022-05-31 09:56:54 +0000

    fix https://github.com/pondersource/sciencemesh-nextcloud/issues/62#issuecomment-1141884665

--- scripts/start-from-Revanc.sh
+++ scripts/start-from-Revanc.sh
@@ -4,7 +4,7 @@ docker run -d --network=testnet --name=revanc1.docker -v /root/reva:/reva -e HOS
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc1.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc1
 
-sleep 10
+sleep 15
 docker exec -it -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data nc1.docker sh /init.sh
 docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revanc1.docker/');"
 docker exec -it maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek nextcloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-1');"
