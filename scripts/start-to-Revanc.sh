47cae6ebc90d217911956171d425b8e6c1e7b784
Michiel de Jong <michiel@unhosted.org>
2022-05-31 12:01:17 +0200

    remove sleep

--- scripts/start-to-Revanc.sh
+++ scripts/start-to-Revanc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker run -d --network=testnet --name=revanc2.docker -v /root/reva:/reva -e HOST=revanc2 revad sleep 30000
+docker run -d --network=testnet --name=revanc2.docker -v /root/reva:/reva -e HOST=revanc2 revad
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=nc2.docker -v /root/nc-sciencemesh:/var/www/html/apps/sciencemesh nc2
 
