db41048e9cf430f5411c5338f3fa1b747bc1c4df
Your Name <you@example.com>
2022-06-14 08:09:13 +0000

    Fix https://github.com/pondersource/OC-Sciencemesh/issues/3

--- servers/owncloud/init.sh
+++ servers/owncloud/init.sh
@@ -4,5 +4,5 @@ php console.php app:enable sciencemesh
 # sed -i "3 i\  'allow_local_remote_servers' => true," config/config.php
 sed -i "8 i\      1 => 'oc1.docker'," /var/www/html/config/config.php
 sed -i "9 i\      2 => 'oc2.docker'," /var/www/html/config/config.php
-sed -i "3 i\  'sharing.managerFactory' => 'OCA\\ScienceMesh\\ScienceMeshProviderFactory'," /var/www/html/config/config.php
+sed -i "3 i\  'sharing.managerFactory' => 'OCA\\\\ScienceMesh\\\\ScienceMeshProviderFactory'," /var/www/html/config/config.php
 
