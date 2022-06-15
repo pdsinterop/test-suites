9d50140048521c2df4b201ea82ac67d95e1a4e4c
Michiel de Jong <michiel@unhosted.org>
2022-06-15 16:46:14 +0200

    debug statements in scripts/start-to-ownCloud.sh

--- scripts/start-to-ownCloud.sh
+++ scripts/start-to-ownCloud.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
+echo "starting with start-to-ownCloud.sh"
+echo "starting maria2.docker"
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+echo "starting oc2.docker"
 docker run -d --network=testnet --name=oc2.docker -v /root/OC-Sciencemesh:/var/www/html/apps/sciencemesh oc2
+echo "sleeping"
 sleep 15
+echo "executing init.sh on oc2.docker"
 docker exec -it -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data oc2.docker sh /init.sh
+echo "executing two mysql commands on maria2.docker"
 docker exec -it maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revaoc2.docker/');"
 docker exec -it maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-2');"
+echo "done with start-to-ownCloud.sh"
