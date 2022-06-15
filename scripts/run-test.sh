87a8cb0879e30573ac5accd0b44b1966b1d39440
Michiel de Jong <michiel@unhosted.org>
2022-06-15 16:42:38 +0200

    Run NC->NC Share-with and add debug info in run-test.sh

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 echo "Testing $1 $2 -> $3"
+echo "Clean..."
 /bin/bash ./scripts/clean.sh
+echo "Start from $2..."
 /bin/bash ./scripts/start-from-$2.sh
+echo "Start to $3..."
 /bin/bash ./scripts/start-to-$3.sh
+echo "Start meshdir..."
 /bin/bash ./scripts/start-meshdir.sh
-
+echo "Run tester"
 docker run --network=testnet --name=tester.docker -e FLOW="$1" -e FROM_SERVER="$2" -e TO_SERVER="$3" ci
