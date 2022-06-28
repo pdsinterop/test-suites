320390bb68822576f4e684eac712139802b93417
Your Name <you@example.com>
2022-06-28 09:43:39 +0000

    repo name oc-sciencemesh lower case

--- scripts/start-to-Revaoc.sh
+++ scripts/start-to-Revaoc.sh
@@ -2,7 +2,7 @@
 set -e
 docker run -d --network=testnet --name=revaoc2.docker -v /root/reva:/reva -e HOST=revaoc2 revad
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
-docker run -d --network=testnet --name=oc2.docker -v /root/OC-Sciencemesh:/var/www/html/apps/sciencemesh oc2
+docker run -d --network=testnet --name=oc2.docker -v /root/oc-sciencemesh:/var/www/html/apps/sciencemesh oc2
 
 sleep 15
 docker exec -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data oc2.docker sh /init.sh
