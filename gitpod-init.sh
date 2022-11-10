5955f0c0ec1b92f9ee7cb86eb85bd58945894635
Michiel de Jong <michiel@unhosted.org>
2022-11-10 10:45:47 +0000

    Build nc/oc app

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -11,5 +11,9 @@ docker run -v /workspace/ocm-test-suite/ocm-stub:/ocm-stub stub npm install
 docker run -v /workspace/ocm-test-suite/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
   --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad"
+docker run -v /workspace/ocm-test-suite/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
+docker run -v /workspace/ocm-test-suite/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
+
 ./scripts/clean.sh
+echo to see the Firefox tester, open `gp url 5800` with your browser
 ./scripts/nrrn-testing.sh
