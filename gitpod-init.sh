ca85db7202ec1e75c7c46185ad8d80c9bb357d65
Michiel de Jong <michiel@unhosted.org>
2022-11-10 18:42:19 +0000

    more build steps

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -11,6 +11,8 @@ git clone --depth=1 --branch=main https://github.com/michielbdejong/ocm-stub
 docker run -v /workspace/ocm-test-suite/ocm-stub:/ocm-stub stub npm install
 docker run -v /workspace/ocm-test-suite/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-  --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad"
+  --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad && make build-reva"
 docker run -v /workspace/ocm-test-suite/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
 docker run -v /workspace/ocm-test-suite/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
+docker pull jlesage/firefox
+docker pull mariadb:latest
