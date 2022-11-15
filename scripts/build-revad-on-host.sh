5ca3f703dc78a6f3a4a6da7e3bbdc6096e4d7472
Michiel de Jong <michielbdejong@users.noreply.github.com>
2022-11-15 13:35:06 +0000

    build-reva scripts

---
+++ scripts/build-revad-on-host.sh
@@ -0,0 +1,4 @@
+
+docker run -v $REPO_PATH/reva:/reva \
+  -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+  --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad"
