b3ccdde86373be0a9aabdb95c92967e44e6b87a6
Michiel de Jong <michielbdejong@users.noreply.github.com>
2022-11-15 13:40:08 +0000

    fix reva build scripts

--- scripts/build-reva-on-host.sh
+++ scripts/build-reva-on-host.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+
+export REPO_ROOT=`pwd`
+[ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
 
 docker run -v $REPO_PATH/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
