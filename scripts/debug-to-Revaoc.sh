1704e6daf12b684abb4f53d1222a3acda27ef0e7
Michiel de Jong <michiel@unhosted.org>
2022-11-16 12:39:24 +0100

    Use waitForPort, don't mount anything in start scripts

--- scripts/debug-to-Revaoc.sh
+++ scripts/debug-to-Revaoc.sh
@@ -4,11 +4,24 @@ set -e
 export REPO_ROOT=`pwd`
 [ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
 
+function waitForPort {
+  x=$(docker exec -it $1 ss -tulpn | grep $2 | wc -l)
+  until [ $x -ne 0 ]
+  do
+    echo Waiting for $1 to open port $2, this usually takes about 10 seconds ... $x
+    sleep 1
+    x=$(docker exec -it $1 ss -tulpn | grep $2 | wc -l)
+  done
+  echo $1 port $2 is open
+}
+
 docker run -d --network=testnet --name=revaoc2.docker -v $REPO_ROOT/reva:/reva -e HOST=revaoc2 revad sleep 30000
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria2.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet --name=oc2.docker -v $REPO_ROOT/oc-sciencemesh:/var/www/html/apps/sciencemesh oc2
 
-sleep 15
+waitForPort maria2.docker 3306
+waitForPort oc2.docker 443
+
 docker exec -e DBHOST=maria2.docker -e USER=marie -e PASS=radioactivity -u www-data oc2.docker sh /init.sh
 docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://revaoc2.docker/');"
 docker exec maria2.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-2');"
--- scripts/start-meshdir.sh
+++ scripts/start-meshdir.sh
@@ -4,4 +4,4 @@ set -e
 export REPO_ROOT=`pwd`
 [ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
 
-docker run -d --network=testnet --name=meshdir.docker  -v $REPO_ROOT/ocm-stub:/ocm-stub stub
+docker run -d --network=testnet --name=meshdir.docker  stub
