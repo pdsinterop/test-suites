7c01a340a7f4f4f9855b61ea5c1e6efc7bfb95bb
Michiel de Jong <michiel@unhosted.org>
2022-11-28 14:15:55 +0000

    scripts

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-REPO_PATH=`pwd`
+REPO_ROOT=`pwd`
 
 git clone --depth=1 --branch=dev https://github.com/sciencemesh/nc-sciencemesh
 git clone --depth=1 --branch=dev https://github.com/pondersource/oc-sciencemesh
@@ -9,11 +9,11 @@ git clone --depth=1 --branch=main https://github.com/michielbdejong/ocm-stub
 
 /bin/bash ./gencerts.sh
 /bin/bash ./rebuild.sh
-docker run -v $REPO_PATH/ocm-stub:/ocm-stub stub npm install
-docker run -v $REPO_PATH/reva:/reva \
+docker run -v $REPO_ROOT/ocm-stub:/ocm-stub stub npm install
+docker run -v $REPO_ROOT/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
   --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad && make build-reva"
-docker run -v $REPO_PATH/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
-docker run -v $REPO_PATH/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
+docker run -v $REPO_ROOT/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
+docker run -v $REPO_ROOT/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
 docker pull jlesage/firefox
 docker pull mariadb:latest
--- scripts/build-revad-on-host.sh
+++ scripts/build-revad-on-host.sh
@@ -4,6 +4,6 @@ set -e
 export REPO_ROOT=`pwd`
 [ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
 
-docker run -v $REPO_PATH/reva:/reva \
+docker run -v $REPO_ROOT/reva:/reva \
   -e"PATH=/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
   --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad"
