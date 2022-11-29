c2035c6f6618426f94810069ee04de6fe619d859
Michiel de Jong <michiel@unhosted.org>
2022-11-29 13:11:46 +0000

    read webdav host from config

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+cd nc-sciencemesh
+git pull
+cd ..
+cd oc-sciencemesh
+git pull
+cd ..
+cd ocm-stub
+git pull
+cd ..
+cd reva
+git pull
+cd ..
+
 ./scripts/clean.sh
 echo to see the Firefox tester, open `gp url 5800` with your browser
 ./scripts/nrrn-testing.sh
