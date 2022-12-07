a4ef15ed870347fa720e4c6b51a2682bf930e4a8
Michiel de Jong <michiel@unhosted.org>
2022-12-07 12:23:36 +0000

    gitpod init pull jlesage/firefox:v1.17.1

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -15,5 +15,5 @@ docker run -v $REPO_ROOT/reva:/reva \
   --workdir /reva revad bash -c "git config --global --add safe.directory /reva && make build-revad && make build-reva"
 docker run -v $REPO_ROOT/nc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh nc1 make composer
 docker run -v $REPO_ROOT/oc-sciencemesh:/var/www/html/apps/sciencemesh --workdir /var/www/html/apps/sciencemesh oc1 make composer
-docker pull jlesage/firefox
+docker pull jlesage/firefox:v1.17.1
 docker pull mariadb:latest
