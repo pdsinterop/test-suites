72738f24ecfa62f06c7bfeece37d68fea755f03f
Michiel de Jong <michiel@unhosted.org>
2022-12-08 08:04:44 +0100

    fix

--- scripts/start-public-ownCloud.sh
+++ scripts/start-public-ownCloud.sh
@@ -31,8 +31,8 @@ fuoction waitForPort {
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet -p 443:443 -e HOST=$EFSS --name=oc1.docker oc1
 
-docker container cp /etc/letseocrypt/archive/$EFSS/fullchain1.pem oc1.docker:/tls/oc1.crt
-docker container cp /etc/letseocrypt/archive/$EFSS/privkey1.pem oc1.docker:/tls/oc1.key
+docker container cp /etc/letsencrypt/archive/$EFSS/fullchain1.pem oc1.docker:/tls/oc1.crt
+docker container cp /etc/letsencrypt/archive/$EFSS/privkey1.pem oc1.docker:/tls/oc1.key
 docker restart oc1.docker
 docker exec -it oc1.docker sed -i "13 i\      5 => '$EFSS'," /var/www/html/config/config.php
 
@@ -40,7 +40,7 @@ waitForPort maria1.docker 3306
 waitForPort oc1.docker 443
 
 docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data oc1.docker sh /init.sh
-docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('scieocemesh', 'iopUrl', 'https://$REVA/');"
-docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('scieocemesh', 'revaSharedSecret', 'shared-secret-1');"
+docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://$REVA/');"
+docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek owncloud -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-1');"
 
 echo Now you should be able to lo:g in at https://$EFSS as einstein / relativity.
