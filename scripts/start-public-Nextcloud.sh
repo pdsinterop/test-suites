886ca6c4fb493b95e6dd5e9b5d4df749c85156d9
root <root@cloud.pondersource.org>
2023-07-03 15:00:11 +0000

    start-public-Nextcloud.sh with pondersource/dev-stock-nextcloud-sciencemesh image

--- scripts/start-public-Nextcloud.sh
+++ scripts/start-public-Nextcloud.sh
@@ -4,14 +4,14 @@ set -e
 export EFSS=cloud.pondersource.org
 export REVA=mesh.pondersource.org
 
-echo Please edit this file and run the commands one-by-one so you can check if it all works
-exit 0
+# echo Please edit this file and run the commands one-by-one so you can check if it all works
+# exit 0
 
-apt install -y certbot docker.io
-certbot certonly --standalone
-git clone https://github.com/cs3org/ocm-test-suite
-cd ocm-test-suite
-docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
+# apt install -y certbot docker.io
+# certbot certonly --standalone
+# git clone https://github.com/cs3org/ocm-test-suite
+# cd ocm-test-suite
+# docker network inspect testnet >/dev/null 2>&1 || docker network create testnet
 
 export REPO_ROOT=`pwd`
 [ ! -d "./scripts" ] && echo "Directory ./scripts DOES NOT exist inside $REPO_ROOT, are you running this from the repo root?" && exit 1
@@ -27,23 +27,27 @@ function waitForPort {
   echo $1 port $2 is open
 }
 
-docker pull pondersource/dev-stock-nc1-sciencemesh
+docker pull pondersource/dev-stock-nextcloud-sciencemesh
+docker tag pondersource/dev-stock-nextcloud-sciencemesh pondersource/dev-stock-nc1-sciencemesh
 docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
 docker run -d --network=testnet -p 443:443 -v $REPO_ROOT/nc-sciencemesh:/var/www/html/apps/sciencemesh -e HOST=$EFSS --name=nc1.docker pondersource/dev-stock-nc1-sciencemesh
+
+docker run -d --network=testnet -e MARIADB_ROOT_PASSWORD=eilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek --name=maria1.docker mariadb --transaction-isolation=READ-COMMITTED --binlog-format=ROW --innodb-file-per-table=1 --skip-innodb-read-only-compressed
+docker run -d --network=testnet -p 443:443 -v $REPO_ROOT/nc-sciencemesh:/var/www/html/apps/sciencemesh -e HOST=$EFSS --name=nc1.docker pondersource/dev-stock-nextcloud-solid
 
 # dereference symlinks to /etc/letsencrypt/archive/$EFSS/fullchain*.pem
 # and /etc/letsencrypt/archive/$EFSS/privkey*.pem,
 # then safely copy them into the container from a full fs path
 cp /etc/letsencrypt/live/$EFSS/fullchain.pem /root/fullchain.pem
 cp /etc/letsencrypt/live/$EFSS/privkey.pem /root/privkey.pem
-docker container cp /root/fullchain.pem nc1.docker:/tls/nc1.crt
-docker container cp /root/privkey.pem nc1.docker:/tls/nc1.key
+docker container cp /root/fullchain.pem nc1.docker:/tls/$EFSS.crt
+docker container cp /root/privkey.pem nc1.docker:/tls/$EFSS.key
 docker restart nc1.docker
 
 waitForPort maria1.docker 3306
 waitForPort nc1.docker 443
 
-docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data nc1.docker sh /init.sh
+docker exec -e DBHOST=maria1.docker -e USER=einstein -e PASS=relativity  -u www-data nc1.docker sh /nc-init.sh
 docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'iopUrl', 'https://$REVA/');"
 docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'revaSharedSecret', 'shared-secret-1');"
 docker exec maria1.docker mariadb -u root -peilohtho9oTahsuongeeTh7reedahPo1Ohwi3aek efss -e "insert into oc_appconfig (appid, configkey, configvalue) values ('sciencemesh', 'inviteManagerApikey', 'shared-secret-1');"
